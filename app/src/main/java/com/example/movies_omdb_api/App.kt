9822ab08a42f87ce2d302815f23e5f06414ffe9b
guntur <laksamana.guntur24@gmail.com>
2021-08-11 15:27:39 +0700

    1. remove jcenter 2. add android.enableJetifier 3. move MainActivity.kt to ui folder 4. add internet permission 5. increment some dependencies version 6. add application class

---
+++ app/src/main/java/com/example/movies_omdb_api/App.kt
@@ -0,0 +1,18 @@
+package com.example.movies_omdb_api
+
+import android.app.Activity
+import android.app.Application
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
+
+class App : Application(), HasActivityInjector {
+    @Inject
+    internal lateinit var activityDispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+
+    override fun activityInjector() = activityDispatchingAndroidInjector
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/example/movies_omdb_api/ui/MainActivity.kt
+++ app/src/main/java/com/example/movies_omdb_api/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.movies_omdb_api
+package com.example.movies_omdb_api.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movies_omdb_api.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
